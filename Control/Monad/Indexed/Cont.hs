f9bf98f8412fcbdb546be66f76d43506a0aa6d1b
Josef Svenningsson <josef.svenningsson@gmail.com>
2015-12-29 18:53:20 +0100

    Cleanup

--- Control/Monad/Indexed/Cont.hs
+++ Control/Monad/Indexed/Cont.hs
@@ -9,11 +9,3 @@ data ContIx r o a where
 instance IxMonad ContIx where
   ireturn a = ContIx $ \k -> k a
   ContIx f `ibind` g = ContIx $ \k -> f $ \a -> unCont (g a) k
-
-data ContIxT m i j k l o r a where
-  ContIxT :: { unContT :: (a -> m i j o) -> m k l r } ->
-    ContIxT m i j k l o r a
-
-ret a = ContIxT $ \k -> k a
-
-bin (ContIxT m) f = ContIxT $ \k -> m $ \a -> unContT (f a) k
