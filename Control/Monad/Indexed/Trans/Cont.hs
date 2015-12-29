2681b74b0a4bb0fe7438e2044afc687c63e3a568
Josef Svenningsson <josef.svenningsson@gmail.com>
2015-12-29 18:55:52 +0100

    Continuation monad transformer!
    This one took quite a while to get right and I'm mightily pleased that
    I managed to push it throught the type checker at all.
    
    The key trick turned out to be to add an extra parameter to the data type
    which tracks intermediate state. For a while I thought it was possible
    to hide it using forall quantification but that turned out not to work
    when control operators was involved.

--- Control/Monad/Indexed/Trans/Cont.hs
+++ Control/Monad/Indexed/Trans/Cont.hs
@@ -1,17 +1,33 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs      #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds  #-}
 module Control.Monad.Indexed.Trans.Cont where
 
--- I don't think it's possible to define a sensible version
--- of the continuation monad transformer. It's just not clear
--- that we have enough type parameters for the two occurrences
--- of the monad being transformed.
-
 import Control.Monad.Indexed
+import Control.Monad.Indexed.Trans
 import Control.Monad.Indexed.Trans.TF
 
-data ContIxT m i j a where
-  ContIxT :: { unContT :: ((a -> m (Snd i) (Snd i) (Fst i)) -> m (Snd j) (Snd j) (Fst j)) } -> ContIxT m i j a
+data ContIxT k m i j a where
+  ContIxT :: { unContT :: (a -> m (Snd j) k  (Fst j)) -> m (Snd i) k (Fst i)
+             } -> ContIxT k m i j a
 
-instance IxMonad m => IxMonad (ContIxT m) where
+instance IxMonad m => IxMonad (ContIxT k m) where
   ireturn a = ContIxT $ \k -> k a
-  ContIxT m `ibind` f = ContIxT $ \k -> m $ \a -> unContT (f a) k
+  ContIxT m `ibind` f = ContIxT (\k -> m (\a -> unContT (f a) k))
+
+instance IxMonadTrans (ContIxT k) where
+  ilift m = ContIxT $ \k -> m `ibind` k
+
+runContIxT :: IxMonad m => ContIxT j m '(s,i) '(t,j) t -> m i j s
+runContIxT (ContIxT f) = f ireturn
+
+callcc :: ((a -> ContIxT k m '(t,j) '(u,n) b) -> ContIxT k m '(s,i) '(t,j) a)
+       -> ContIxT k m '(s,i) '(t,j) a
+callcc f = ContIxT (\ c -> unContT (f (\ x -> ContIxT (\ _ -> c x))) c)
+
+shift :: IxMonad m => ((a -> m j k t) -> ContIxT k m '(s,i) '(b,k) b)
+                   -> ContIxT k m '(s,i) '(t,j) a
+shift f = ContIxT (runContIxT . f)
+
+reset :: IxMonad m => ContIxT j m '(a,i) '(t1,j) t1 -> ContIxT k m '(s,i) '(s,j) a
+reset = ilift . runContIxT
