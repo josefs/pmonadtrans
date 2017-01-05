b63f8499df3e2d43af59ead30ee357954bf2ac2b
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-01-05 08:55:56 +0100

    The Writer class finally works! Sadly no kind poly
    The solution to making the writer class work was to
    monomorphize the kinds of the indices of the writer
    monad transformer. This is a bit sad and I should ask
    people whether it's possible to lift that restriction.
    But it works for now, albeit with somewhat limited
    functionality.

--- Control/Monad/Param/Writer.hs
+++ Control/Monad/Param/Writer.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE PolyKinds      #-}
-{-# LANGUAGE TypeFamilies   #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GADTs                  #-}
+{-# LANGUAGE KindSignatures         #-}
+{-# LANGUAGE PolyKinds              #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
 {-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE UndecidableInstances   #-}
 module Control.Monad.Param.Writer where
 
 import Prelude hiding (id,(.))
@@ -13,13 +14,20 @@ import Type.Pair
 import Control.Monad.Param
 import Control.Monad.Param.Trans
 
+import Control.Monad.Param.State
+
 import Control.Category
 
 import GHC.Exts
 
 data WriterPT c m i j a where
-  WriterPT :: { runWriterPT :: m (Snd i) (Snd j) (a,c (Fst i) (Fst j)) } ->
+  WriterPT :: { runWriterPT :: m (Snd i) (Snd j)
+                                 (a,c (Fst i :: *) (Fst j :: *)) } ->
                WriterPT c m i j a 
+-- I'd rather avoid forcing the kind of c to be * -> * -> *
+-- but I don't know how to make a monad transformer when it is
+-- kind polymorphic
+
 
 instance PFunctor f => PFunctor (WriterPT c f) where
   pfmap f (WriterPT m) = WriterPT $ pfmap (\(a,c) -> (f a,c)) m
@@ -61,49 +69,22 @@ pass
      -> WriterPT c m k l a
 pass (WriterPT m) = WriterPT $ m `pbind` \((a,p),w) -> preturn (a,p w)
 
-type family Cat (m :: k -> k -> * -> *) :: k' -> k' -> *
-type family Acc (m :: l -> l -> * -> *) (a :: k) :: k'
-type family NotAccEq (m :: k -> k -> * -> *) (a :: k) (b :: k) :: Constraint
-
 class Writer (m :: k -> k -> * -> *) where
-{-
-  type Cat m :: k' -> k' -> *
-  type Acc m (a :: k) :: k'
-  type NotAccEq m (a :: k) (b :: k) :: Constraint
--}
+  type Cat (m :: k -> k -> * -> *) :: * -> * -> *
+  type Acc (m :: k -> k -> * -> *) (a :: k) :: *
+  type NotAccEq (m :: k -> k -> * -> *) (a :: k) (b :: k) :: Constraint
   tellW :: NotAccEq m a b => (Cat m) (Acc m a) (Acc m b) -> m a b ()
 
-type instance Cat (WriterPT c m) = c
-type instance Acc (WriterPT c m) a = Fst a
-type instance NotAccEq (WriterPT c m) a b = Snd a ~ Snd b
-{-
 instance PMonad m => Writer (WriterPT c m) where
-  {-
   type Cat (WriterPT c m) = c
   type Acc (WriterPT c m) a = Fst a
   type NotAccEq (WriterPT c m) a b = Snd a ~ Snd b
--}
   tellW c = WriterPT (preturn ((),c))
--}
-{-
-data WriterM c i j a where
-  WriterM :: { runWriterM :: (a,c i j) } -> WriterM c i j a
 
-type instance Cat (WriterM c) = c
-type instance Acc (WriterM c) a = a
+instance (State m, PMonad m, Category c) => State (WriterPT c m) where
+  get = WriterPT (get `pbind` \s -> preturn (s,id))
+  set s = WriterPT (set s `pbind` \_ -> preturn ((),id))
 
-type instance NotAccEq (WriterM c) a b = ()
-
---instance Writer (WriterM c) where
---  tellW c = WriterM ((),c)
-
-
-class Writer' m c | m -> c where
-  tellW' :: (NotAccEq m a b, Acc m i ~ a, Acc m j ~ b) => c i j -> m a b ()
-
-instance Writer' (WriterM c) c where
-  tellW' c = WriterM ((),c)
-
-instance Writer' (WriterPT c m) c where
-  tellW' c = WriterPT (preturn ((),c))
--}
+type instance GetState   (WriterPT c m) a   = GetState m (Snd a)
+type instance NotStateEq (WriterPT c m) a b = (Fst a ~ Fst b
+                                              ,NotStateEq m (Snd a) (Snd b))
