751a5fd9b5600f1346aa70d5705ba718d70ae2f9
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-02-11 12:10:01 +0100

    Simplified callcc
    It turns out that the type of callcc can be simplified
    quite a bit. There is no need to keep track of the two
    different levels parameters because they are always in sync.

--- Control/Monad/Param/Cont.hs
+++ Control/Monad/Param/Cont.hs
@@ -1,24 +1,21 @@
 {-# LANGUAGE GADTs      #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds  #-}
+{-# LANGUAGE PolyKinds  #-}
 module Control.Monad.Param.Cont where
 
 import Type.Pair
 import Control.Monad.Param
 import Control.Monad.Param.Trans
 
-{- I can't say I understand this type. In particular I don't understand
-   why the second parameter of 'm' can be anything but the first one needs
-   to be something very particular.
-
-   Well, thinking about it a little here's one way to interpret it.
-   The continuation is what is going to happen after the current computation,
-   so therefore the post-state of the current computation is the pre-state
-   of the continuation. And the continuation runs all the way to the end
-   and the end state is k. Now the monad computation we return it the thing
-   that is going to the run right now and hence it must accept the current
-   pre-state as a pre-state. And it will run until the end of time which is
-   why it has k as it post-state.
+{- Here's one way to interpret this very complicated type.
+   The continuation is what is going to happen after the current
+   computation, so therefore the post-state of the current computation
+   is the pre-state of the continuation. And the continuation runs all
+   the way to the end and the end state is k. Now the monad computation
+   we return it the thing that is going to the run right now and hence
+   it must accept the current pre-state as a pre-state. And it will
+   run until the end of time which is why it has k as it post-state.
 -}
 
 data ContPT k m i j a where
@@ -43,8 +40,8 @@ instance PMonadTrans (ContPT k) where
 runContPT :: PMonad m => ContPT j m '(s,i) '(t,j) t -> m i j s
 runContPT (ContPT f) = f preturn
 
-callcc :: ((a -> ContPT k m '(t,j) '(u,n) b) -> ContPT k m '(s,i) '(t,j) a)
-       -> ContPT k m '(s,i) '(t,j) a
+callcc :: ((a -> ContPT k m j n b) -> ContPT k m i j a)
+       -> ContPT k m i j a
 callcc f = ContPT (\ c -> unContT (f (\ x -> ContPT (\ _ -> c x))) c)
 
 shift :: PMonad m => ((a -> m j k t) -> ContPT k m '(s,i) '(b,k) b)
@@ -53,3 +50,10 @@ shift f = ContPT (runContPT . f)
 
 reset :: PMonad m => ContPT j m '(a,i) '(t,j) t -> ContPT k m '(s,i) '(s,j) a
 reset = plift . runContPT
+
+class Cont m where
+  callccC :: ((a -> m j n b) -> m i j a) -> m i j a
+
+instance Cont (ContPT k m) where
+  callccC = callcc
+
